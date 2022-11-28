4ac29ecb7fdf5aefc6403bd8b201dbdb98337960
Damien Sedgwick <damienksedgwick@gmail.com>
2022-11-28 16:53:45 +0000

    mocked out route handlers for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, put, delete, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/api/v1/todos")]
+async fn get_todos() -> impl Responder {
+    HttpResponse::Ok().body("Get all todos")
+}
+
+#[get("/api/v1/todos/{id}")]
+async fn get_todo_by_id(id: String) -> impl Responder {
+    HttpResponse::Ok().body("Get a todo by id")
+}
+
+#[post("/api/v1/todos")]
+async fn create_todo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body("Create a todo")
+}
+
+#[put("/api/v1/todos/{id}")]
+async fn update_todo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body("Update a todo")
+}
+
+#[delete("/api/v1/todos/{id}")]
+async fn delete_todo(id: String) -> impl Responder {
+    HttpResponse::Ok().body("Delete a todo")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(get_todos)
+            .service(get_todo_by_id)
+            .service(create_todo)
+            .service(update_todo)
+            .service(delete_todo)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
