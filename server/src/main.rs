9f797cc55d04c1a2589d75eecf9ed21a626d2c48
Damien Sedgwick <damienksedgwick@gmail.com>
2022-11-28 22:00:08 +0000

    implemented get all todos route with: Tom, Tim, Will, Liam, Chris and another Chris

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,50 @@
-use actix_web::{get, post, put, delete, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{delete, get, http::header::ContentType, post, put, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use sqlx::sqlite::{SqliteConnection};
+use sqlx::Connection;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Todo {
+    id: i64,
+    title: String,
+    completed: bool,
+    archived: bool,
+}
+
+#[derive(sqlx::FromRow)]
+struct TodoRow {
+    id: i64,
+    title: String,
+    completed: Option<i64>,
+    archived: Option<i64>,
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
 
 #[get("/api/v1/todos")]
 async fn get_todos() -> impl Responder {
-    HttpResponse::Ok().body("Get all todos")
+    let mut conn = SqliteConnection::connect("sqlite:todos.db").await.unwrap();
+
+    let rows = sqlx::query_as!(TodoRow, "SELECT * FROM todos")
+        .fetch_all(&mut conn)
+        .await
+        .unwrap();
+
+    let todos = rows
+        .into_iter()
+        .map(|row| Todo {
+            id: row.id,
+            title: row.title.clone(),
+            completed: row.completed.unwrap_or(0) == 1,
+            archived: row.archived.unwrap_or(0) == 1,
+        })
+        .collect::<Vec<Todo>>();
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(serde_json::to_string(&todos).unwrap())
 }
 
 #[get("/api/v1/todos/{id}")]
@@ -25,7 +67,6 @@ async fn delete_todo() -> impl Responder {
     HttpResponse::Ok().body("Delete a todo")
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -36,7 +77,7 @@ async fn main() -> std::io::Result<()> {
             .service(update_todo)
             .service(delete_todo)
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
