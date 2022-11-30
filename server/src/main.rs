ff4836b85a599aaf0cf95186266f48aeb177adb4
Damien Sedgwick <damienksedgwick@gmail.com>
2022-11-30 19:24:03 +0000

    retrieving a todo by id works using the bulk of the previous code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,10 @@
 use actix_cors::Cors;
-use actix_web::{delete, get, http::header::ContentType, post, put, App, HttpResponse, HttpServer, Responder, http};
+use actix_web::{
+    delete, get, http, http::header::ContentType, post, put, web, App, HttpResponse, HttpServer,
+    Responder,
+};
 use serde::{Deserialize, Serialize};
-use sqlx::sqlite::{SqliteConnection};
+use sqlx::sqlite::SqliteConnection;
 use sqlx::Connection;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -45,8 +48,35 @@ async fn get_todos() -> impl Responder {
 }
 
 #[get("/api/v1/todos/{id}")]
-async fn get_todo_by_id() -> impl Responder {
-    HttpResponse::Ok().body("Get a todo by id")
+async fn get_todo_by_id(path: web::Path<String>) -> impl Responder {
+    let id = path.into_inner();
+
+    let mut conn = SqliteConnection::connect("sqlite:todos.db").await.unwrap();
+
+    let rows = sqlx::query_as!(TodoRow, "SELECT * FROM todos WHERE id =?", id)
+        .fetch_all(&mut conn)
+        .await
+        .unwrap();
+
+    let todos = rows
+        .into_iter()
+        .map(|row| Todo {
+            id: row.id,
+            title: row.title.clone(),
+            completed: row.completed.unwrap_or(0) == 1,
+            archived: row.archived.unwrap_or(0) == 1,
+        })
+        .collect::<Vec<Todo>>();
+
+    if !todos.is_empty() {
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(serde_json::to_string(&todos).unwrap())
+    } else {
+        HttpResponse::NotFound()
+            .content_type(ContentType::json())
+            .body("Unable to find todo")
+    }
 }
 
 #[post("/api/v1/todos")]
@@ -69,9 +99,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let cors = Cors::default()
             .allowed_origin("http://localhost:5173")
-            .allowed_origin_fn(|origin, _req_head| {
-                origin.as_bytes().ends_with(b".localhost:5173")
-            })
+            .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b".localhost:5173"))
             .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
@@ -85,7 +113,7 @@ async fn main() -> std::io::Result<()> {
             .service(update_todo)
             .service(delete_todo)
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
